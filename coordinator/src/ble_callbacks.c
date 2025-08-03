9cd5a8a90f32c78fad8472dbd32879d95b91fea0
Agustin Coitinho <coitinho10@gmail.com>
2025-08-03 20:36:42 -0300

    No funciona

--- coordinator/src/ble_callbacks.c
+++ coordinator/src/ble_callbacks.c
@@ -7,7 +7,19 @@
 
 LOG_MODULE_REGISTER(ble_callbacks, LOG_LEVEL_INF);
 
-struct bt_conn *my_conn = NULL;
+static uint8_t active_connections = 0; // Counter for active connections
+#define MAX_CONN CONFIG_BT_MAX_CONN
+
+static struct bt_conn *conns[MAX_CONN] = { 0 };
+
+static void restart_advertising_if_needed(void) {
+    if (active_connections < MAX_CONN) {
+        LOG_INF("Advertising again (active: %d, max: %d)", active_connections, MAX_CONN);
+        ble_advertising_start();
+    } else {
+        LOG_INF("Maximum connections reached: %d", MAX_CONN);
+    }
+}
 
 static void on_connected(struct bt_conn *conn, uint8_t err) {
     if (err) {
@@ -15,8 +27,14 @@ static void on_connected(struct bt_conn *conn, uint8_t err) {
 		return;
 	}
 	LOG_INF("Connected");
-	my_conn = bt_conn_ref(conn);
 	dk_set_led(CONNECTION_STATUS_LED, 1);
+
+	if (active_connections < MAX_CONN) {
+        conns[active_connections++] = bt_conn_ref(conn);  // guardar referencia
+    } else {
+        LOG_WRN("Too many connections already established!");
+    }
+
 	/* STEP 1.1 - Declare a structure to store the connection parameters */
 	struct bt_conn_info info;
 	err = bt_conn_get_info(conn, &info);
@@ -29,6 +47,9 @@ static void on_connected(struct bt_conn *conn, uint8_t err) {
 	uint16_t supervision_timeout = info.le.timeout*10; // in ms
 	LOG_INF("Connection parameters: interval %.2f ms, latency %d intervals, timeout %d ms", connection_interval, info.le.latency, supervision_timeout);
 
+	restart_advertising_if_needed();
+
+	//ble_advertising_start();
 	/* update_phy(my_conn);
 	k_sleep(K_MSEC(1000)); // Delay added to avoid link layer collisions.
 	update_data_length(my_conn);
@@ -38,7 +59,16 @@ static void on_connected(struct bt_conn *conn, uint8_t err) {
 static void on_disconnected(struct bt_conn *conn, uint8_t reason) {
     LOG_INF("Disconnected. Reason %d", reason);
 	dk_set_led(CONNECTION_STATUS_LED, 0);
-	bt_conn_unref(my_conn);
+
+	// Search and delete the connection
+    for (int i = 0; i < MAX_CONN; i++) {
+        if (conns[i] == conn) {
+            bt_conn_unref(conns[i]);
+            conns[i] = NULL;
+            active_connections--;
+            break;
+        }
+    }
 }
 
 static void on_le_param_updated(struct bt_conn *conn, uint16_t interval,
@@ -50,7 +80,8 @@ static void on_le_param_updated(struct bt_conn *conn, uint16_t interval,
 
 static void on_recycled(void)
 {
-	ble_advertising_start();
+	LOG_INF("on_recycled");
+	restart_advertising_if_needed();
 }
 
 /* static void on_le_phy_updated(struct bt_conn *conn, struct bt_conn_le_phy_info *param) {
